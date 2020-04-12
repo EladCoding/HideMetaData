55a7d4c56cdab03929ff21dcf96d8b1a85049a54
EladCoding <eladshoham93@gmail.com>
2020-04-12 15:27:44 +0300

    remove mainpkg, use only the updated mixnet

--- mixnet/createUser.go
+++ mixnet/createUser.go
@@ -2,13 +2,20 @@ package mixnet
 
 import (
 	"fmt"
+	"github.com/EladCoding/HideMetaData/scripts"
 	"strings"
 )
 
 func StartUser(mode string, name string) {
 	switch strings.ToLower(mode) {
 	case "server":
-		StartServer(name)
+		for _, serverName := range scripts.ServerNames {
+			if name == serverName {
+				StartServer(name)
+			}
+			break
+		}
+		fmt.Println("Server name does not Exists!")
 	case "mediator":
 		switch name {
 		case "101":
@@ -18,10 +25,16 @@ func StartUser(mode string, name string) {
 		case "103":
 			StartMediator(name, 3, "001", false, true)
 		default:
-			fmt.Println("You can only chose mediator 101 102 or 103")
+			fmt.Println("Mediator name does not Exists!")
 		}
 	case "client":
-		StartClient(name)
+		for _, serverName := range scripts.ClientNames {
+			if name == serverName {
+				StartClient(name)
+			}
+			break
+		}
+		fmt.Println("Client name does not Exists!")
 	default:
 		fmt.Println("You can only be client mediator or server")
 	}
